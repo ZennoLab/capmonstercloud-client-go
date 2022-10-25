245e2716405503b096f73951cc816307e45d4443
tandemic97 <tandemic97@gmail.com>
2022-10-25 14:58:47 +0300

    add hCaptchaTask

---
+++ pkg/tasks/hCaptchaTask.go
@@ -0,0 +1,52 @@
+package tasks
+
+type hCaptchaTaskProxyless struct {
+	Type        string  `json:"type"`
+	WebsiteURL  string  `json:"websiteURL"`
+	WebsiteKey  string  `json:"websiteKey"`
+	IsInvisible *bool   `json:"isInvisible,omitempty"`
+	Data        *string `json:"data,omitempty"`
+	userAgentAndCookies
+}
+
+func NewHCaptchaTaskProxyless(websiteURL, websiteKey, userAgent string) hCaptchaTaskProxyless {
+	return hCaptchaTaskProxyless{
+		Type:       "HCaptchaTaskProxyless",
+		WebsiteURL: websiteURL,
+		WebsiteKey: websiteKey,
+		userAgentAndCookies: userAgentAndCookies{
+			UserAgent: &userAgent,
+		},
+	}
+}
+
+func (t hCaptchaTaskProxyless) WithIsInvisible(isInvisible bool) hCaptchaTaskProxyless {
+	t.IsInvisible = &isInvisible
+	return t
+}
+
+func (t hCaptchaTaskProxyless) WithData(data string) hCaptchaTaskProxyless {
+	t.Data = &data
+	return t
+}
+
+type hCaptchaTask struct {
+	hCaptchaTaskProxyless
+	taskProxy
+	userAgentAndCookies
+}
+
+func NewHCaptchaTask(websiteURL, websiteKey, proxyType, proxyAddress string, proxyPort int) hCaptchaTask {
+	return hCaptchaTask{
+		hCaptchaTaskProxyless: hCaptchaTaskProxyless{
+			Type:       "HCaptchaTask",
+			WebsiteURL: websiteURL,
+			WebsiteKey: websiteKey,
+		},
+		taskProxy: taskProxy{
+			ProxyType:    proxyType,
+			ProxyAddress: proxyAddress,
+			ProxyPort:    proxyPort,
+		},
+	}
+}
